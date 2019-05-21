b169b586b236a8dd6a1150b855382ce1465251c3
Stephen Binns <stephen.binns@gmail.com>
2019-05-21 21:44:25 +0100

    Move dist to docs

---
+++ docs/precache-manifest.91d883e83528cd8d8f0ac2f7cd3c14f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56d75f979658e26f6ae7fc665a254f2",
+    "url": "assets/shrug.png"
+  },
+  {
+    "revision": "fd950504669d9d990a6023e6a3e7e344",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "6f62c9dac14013d87132",
+    "url": "vendors.052e2929d8eb6af92d3b.bundle.js"
+  },
+  {
+    "revision": "dc2d4e1c088d973ebdafaef2aa406479",
+    "url": "assets/wall.png"
+  },
+  {
+    "revision": "d10b3a0f3adb43abb1f1",
+    "url": "main.69f6687102c0b326bce2.bundle.js"
+  },
+  {
+    "revision": "034c5fd767e1dd5b09548a8306cfa2da",
+    "url": "index.html"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "1d2345a5a199f580139a6efadcc702e7",
+    "url": "assets/table.png"
+  },
+  {
+    "revision": "f314534f3e632ba76dea274643dd6b43",
+    "url": "assets/player2.png"
+  },
+  {
+    "revision": "87478d57b05708b11ddf62f67057ad9a",
+    "url": "assets/player1.png"
+  },
+  {
+    "revision": "2308810d062cfcf686120879e1afec95",
+    "url": "assets/player1-down.png"
+  },
+  {
+    "revision": "d57b8bbbdb42208cdd0b079b878925e8",
+    "url": ".DS_Store"
+  }
+]);
---
+++ docs/sw.js
@@ -0,0 +1,11 @@
+importScripts("precache-manifest.91d883e83528cd8d8f0ac2f7cd3c14f0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
